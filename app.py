6abfab21291b75a920d7919c673647891a1db932
Advaith Suresh <56351354+Advaith123@users.noreply.github.com>
2021-04-26 15:19:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def home():
     return d.final_df.to_json()
 
+
+@app.route('/')
+def plswork():
+    return "<h1>Hello World!!!!!!!!!!!!!!!!!!!!!!!!!</h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
